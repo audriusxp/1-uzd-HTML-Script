3208a9364768edecc2054918d46765b0fce41556
audriusxp <palm@inbox.lt>
2022-06-20 19:46:32 +0300

    atlikta uzduotis

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 window.onload = function() {
-}
     
+    let ilgioNuskaitymoLangelis = document.getElementById("ilgis");
+    console.log(ilgioNuskaitymoLangelis);
+    let plocioIvedimoLaukelis = document.getElementById("plotis");
+    console.log(plocioIvedimoLaukelis);
+    let mygtukas = document.getElementById("skaiciavimoMygtukas");
+    console.log(mygtukas);
+    mygtukas.onclick = function () {
+        let ilgis =ilgioNuskaitymoLangelis.valueAsNumber;
+        console.log(ilgis);
+        let plotis = plocioIvedimoLaukelis.valueAsNumber;
+        console.log(plotis);
+        console.log(staciakampioPerimetras(ilgis,plotis));
+        rezultatoPastraipa.innerHTML = "Stačiakampio perimetras yra: " + staciakampioPerimetras(ilgis, plotis);
+        
+    }
+    
+    
+    }
+
+ function staciakampioPerimetras(a,b){
+     return 2*a + 2*b;
+ }
